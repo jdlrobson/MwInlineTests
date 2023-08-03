87dcb43c04048d5c5b928e9c096b5807a7c62191
Jon Robson <jdlrobson@gmail.com>
2023-08-02 18:22:36 -0700

    Update inline.test.js
    Update based on chat with Roan to include the -clientpref- suffix.

--- inline.test.js
+++ inline.test.js
@@ -19,7 +19,7 @@ describe( 'The MediaWiki inline script', () => {
 		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js' );
 	} );
 
-	test( 'Not integer suffixs are ignored.', () => {
+	test( 'Features missing -clientpref- suffix are ignored.', () => {
 		// set cookie and body classes
 		document.documentElement.setAttribute( 'class', 'client-js vector-feature-zebra-disabled' );
 		document.cookie = makeCookie( {
@@ -31,51 +31,51 @@ describe( 'The MediaWiki inline script', () => {
 
 	test( 'Features can be toggled on', () => {
 		// set cookie and body classes
-		document.documentElement.setAttribute( 'class', 'client-js vector-limited-width-0' );
+		document.documentElement.setAttribute( 'class', 'client-js vector-limited-width-clientpref-0' );
 		document.cookie = makeCookie( {
 			'vector-limited-width': 1
 		} );
 		inline( PREFIX );
-		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-limited-width-1' );
+		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-limited-width-clientpref-1' );
 	} );
 
 	test( 'Features can be toggled off', () => {
 		// set cookie and body classes
-		document.documentElement.setAttribute( 'class', 'client-js vector-limited-width-1' );
+		document.documentElement.setAttribute( 'class', 'client-js vector-limited-width-clientpref-1' );
 		document.cookie = makeCookie( {
 			'vector-limited-width': 0
 		} );
 		inline( PREFIX );
-		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-limited-width-0' );
+		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-limited-width-clientpref-0' );
 	} );
 
 	test( 'Can toggle from font size 16 to 32', () => {
 		// set cookie and body classes
-		document.documentElement.setAttribute( 'class', 'client-js vector-font-size-16' );
+		document.documentElement.setAttribute( 'class', 'client-js vector-font-size-clientpref-16' );
 		document.cookie = makeCookie( {
 			'vector-font-size': 32
 		} );
 		inline( PREFIX );
-		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-font-size-32' );
+		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-font-size-clientpref-32' );
 	} );
 
 	test( 'Can toggle from font size 32 to 16', () => {
 		// set cookie and body classes
-		document.documentElement.setAttribute( 'class', 'client-js vector-font-size-32' );
+		document.documentElement.setAttribute( 'class', 'client-js vector-font-size-clientpref-32' );
 		document.cookie = makeCookie( {
 			'vector-font-size': 16
 		} );
 		inline( PREFIX );
-		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-font-size-16' );
+		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-font-size-clientpref-16' );
 	} );
 
-	test( 'Do not modify similar classes', () => {
+	test( 'Values cannot contain dashes', () => {
 		// set cookie and body classes
-		document.documentElement.setAttribute( 'class', 'client-js vector-font-32-foo' );
+		document.documentElement.setAttribute( 'class', 'client-js vector-font-clientpref-32-foo' );
 		document.cookie = makeCookie( {
 			'vector-font': 16
 		} );
 		inline( PREFIX );
-		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-font-32-foo' );
+		expect( document.documentElement.getAttribute( 'class' ) ).toBe( 'client-js vector-font-clientpref-32-foo' );
 	} );
 } );
