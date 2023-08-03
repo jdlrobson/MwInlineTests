a1ecc7049b0f9a55f71f859fbac049b1cd035146
Moh'd Khier Abualruz <mkh.ruzz@live.com>
2023-08-03 20:23:27 +0300

    update inline.js

--- inline.js
+++ inline.js
@@ -2,12 +2,12 @@
 // Copy the JS code from ClientHtml.php into the function body.
 // Run npx jest tests/jest/inline.test.js
 function mwInlineScript( cookiePrefix ) {
-	var cookie = document.cookie.match( new RegExp( '/(?:^|; )' + cookiePrefix + 'mwclientprefs=([^;]+)/' ) );
-	// cookie match is a csv string of the form "key1=value1,key2=value2"
-	var prefArray = cookie && cookie[1] ? cookie[1].split( '%2C' ) : [];
+	var cookie = document.cookie.match( /(?:^|; )mwclientpreferences=([^;]+)/ );
+	// cookie match is a ! seperated pairs with the pair seperator ~ "key1~value1!key2~value2"
+	var prefArray = cookie && cookie[1] ? cookie[1].split( '!' ) : [];
 	var clientPreferences = {};
 	for ( var i = 0; i < prefArray.length; i++ ) {
-		var pair = prefArray[i].split( '%3D' );
+		var pair = prefArray[i].split( '~' );
 		clientPreferences[pair[0]] = pair[1];
 	}
 	// Only uses existing classes, for maintainability
@@ -15,19 +15,17 @@ function mwInlineScript( cookiePrefix ) {
 	// so that the feature class is already in the cache when the feature is deployed
 	// regex explanation:
 	// (^| ) = start of string or space
-	// ([^ -]+-[^ ]+-) = two parts prefix separated by a hyphen, each part containing at least
-	// one non-hyphen and non-space character and ending with a hyphen (mw-pref- or vector-feature-)
 	// ([^ ]+) = one or more non-space characters
-	// (-enabled|disabled|\d+) = -enabled or -disabled or a number
+	// -clientpref- = literal string
+	// [a-zA-Z0-9]+ = one or more alphanumeric characters
 	// Regex replace examples:
-	// "mw-pref-foo-bar-enabled" -> "mw-pref-foo-bar-0"
-	// "vector-feature-foo-bar-2" -> "vector-feature-foo-bar-0"
-	// "mw-pref-foo-bar-2" -> "mw-pref-foo-bar-4"
+	// "vector-feature-foo-clientpref-0" -> "vector-feature-foo-clientpref-1"
+	// "mw-pref-bar-clientpref-2" -> "mw-pref-bar-clientpref-4"
 	document.documentElement.className = document.documentElement.className.replace(
-		/(^| )([^ -]+-[^ ]+-)([^ ]+)-(enabled|disabled|\d+)/g,
-		function ( match, before, prefix, key, value ) {
+		/(^| )([^ ]+)-clientpref-[a-zA-Z0-9]+( |$)/g,
+		function ( match, before, key, after ) {
 			if ( clientPreferences.hasOwnProperty( key ) ) {
-				return before + prefix + key + '-' + clientPreferences[ key ];
+				return before + key + '-clientpref-' + clientPreferences[ key ] + after;
 			}
 			return match;
 		}
